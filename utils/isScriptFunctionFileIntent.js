f6bdf91f45be5889b3006ce4a6b54d5299407ed7
m.lubahn <m.lubahn@klippel.de>
2023-05-17 10:55:21 +0200

    Restructuring cause of packaging error

---
+++ utils/isScriptFunctionFileIntent.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.isScriptFunctionFileIntent = void 0;
+const lodash_1 = require("lodash");
+const isScriptFunctionFileIntent = (result) => (0, lodash_1.isObject)(result);
+exports.isScriptFunctionFileIntent = isScriptFunctionFileIntent;
+//# sourceMappingURL=isScriptFunctionFileIntent.js.map
---
+++ utils/shouldUpdateCurrentTextSelection.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.shouldUpdateCurrentTextSelection = void 0;
+const shouldUpdateCurrentTextSelection = (transformed) => transformed !== undefined && transformed !== null && transformed !== false;
+exports.shouldUpdateCurrentTextSelection = shouldUpdateCurrentTextSelection;
+//# sourceMappingURL=shouldUpdateCurrentTextSelection.js.map
